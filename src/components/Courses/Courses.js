eb346f3de5b5dc44214d59da0e7c0095771bcd0a
Sadikul Islam <sh.sifatallrafi.08@gmail.com>
2022-10-26 21:48:39 +0600

    routing added

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -9,13 +9,25 @@ const Courses = () => {
     console.log(allSubjects);
 
     return (
-        <div style={{minHeight: '100vh'}}>
+        <div style={{minHeight: '100vh'}} className=''>
+
+            <div style={{height: '35vh', width: '100%'}} className='bg-light d-flex  justify-content-center align-items-center'>
+                <div className='text-center w-100 container'>
+                    <p className='fs-1'>Learn</p>
+                    <p className='fs-5'>Explore our structured learning paths to discover everything you need to know about building for the modern web.</p>
+                </div>
+            </div>
+
+         <div className='container'>
+         <div className=' row row-cols-1  row-cols-md-3 mt-5 g-4'>
           {
             allSubjects.map(subject => <Subjects
             key={subject.id}
             subject={subject}
             ></Subjects>)
           }
+          </div>
+         </div>
         </div>
     );
 };
--- src/components/Courses/Subjects/Subjects.js
+++ src/components/Courses/Subjects/Subjects.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { FaGraduationCap } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import './Subjects.css'
 
 const Subjects = ({subject}) => {
     const {achivment, banner, description, subject_name} = subject;
     return (
         <div>
-            
+            <div style={{height: '310px'}} className="col subjects-card mb-3">
+    <div className="card h-100 p-2">
+        <div className='d-flex justify-content-between'>
+            <p>Course</p>
+            <p className='bg-primary py-1 text-white px-3 rounded-pill'> {achivment}<FaGraduationCap className='ms-1 mb-1'></FaGraduationCap> </p>
+        </div>
+      <img style={{height: '170px'}} src={banner} class="card-img-top" alt="..."/>
+      <div className="card-body p-0">
+        
+        <p className="card-text">{description.length > 200 ?
+        <p>{description.slice(0, 80) + '...'} <Link>Explore more</Link> </p>
+        :
+        <p>{description}</p>
+    }</p>
+      </div>
+    </div>
+  </div>
         </div>
     );
 };
